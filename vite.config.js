106775b8edd91df1f6c68dcf57d175450e6fd986
Jazeel Abdulmajeed <38857675+majeedson@users.noreply.github.com>
2026-09-24 16:42:46 +0300

    Build 62: enforce hashed deployment assets

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/chaitracker/'
+  base: '/chaitracker/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: { output: { entryFileNames: 'assets/[name]-[hash].js', chunkFileNames: 'assets/[name]-[hash].js', assetFileNames: 'assets/[name]-[hash][extname]' } }
+  }
 });
